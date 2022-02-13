2191b38b02f9170ddb6dfa8a37942ceda0476ab7
cascooscuro <cascooscuro@users.noreply.github.com>
2022-02-13 12:08:04 +0100

    Update spotyzam.go

--- spotyzam.go
+++ spotyzam.go
@@ -36,36 +36,34 @@ var (
 
 func main() {
 
+
         helptext:= "ERROR: invalid option use 'spotyzam playlist-id csv-file.csv' \n" +
-  " Instructions: \n" + 
-  "1. Register an application at: https://developer.spotify.com/my-applications/ \n" + 
-   "   - Use 'http://localhost:8080/callback' as the redirect URI \n" +
-  "2. Set the SPOTIFY_ID environment variable to the client ID you got in step 1. \n" +
-     "set SPOTIFY_ID=your-spotify-client-id  (from windows cmd) \n" +
-     "export SPOTIFY_ID=your-spotify-client-id (linux shell) \n" +
-  "3. Set the SPOTIFY_SECRET environment variable to the client secret from step 1. \n" +
-     "set SPOTIFY_SECRET=your-spotify-client-secret  (from windows cmd) \n" +
-     "export SPOTIFY_SECRET=your-spotify-client-secret (linux shell) \n" +
-  "4. Create a playlist where you want your tracks to be added \n" + 
-  "    get the ID of that playlist by using the share button. \n" + 
-  "    The ID is the string that goes after https://open.spotify.com/playlist/ \n" + 
-  "    e.g.  in 'https://open.spotify.com/playlist/37i9dQZF1DX6YTj07PjLwE' the id is 37i9dQZF1DX6YTj07PjLwE  \n" + 
-  "5. The csv file expects the format used by shazam: Index,TagTime,Title,Artist,URL,TrackKey. We skip the first line.  \n"
-
-
+        " Instructions: \n" + 
+        "1. Register an application at: https://developer.spotify.com/my-applications/ \n" + 
+        "   - Use 'http://localhost:8080/callback' as the redirect URI \n" +
+        "2. Set the SPOTIFY_ID environment variable to the client ID you got in step 1. \n" +
+        "set SPOTIFY_ID=your-spotify-client-id  (from windows cmd) \n" +
+        "export SPOTIFY_ID=your-spotify-client-id (linux shell) \n" +
+        "3. Set the SPOTIFY_SECRET environment variable to the client secret from step 1. \n" +
+        "set SPOTIFY_SECRET=your-spotify-client-secret  (from windows cmd) \n" +
+        "export SPOTIFY_SECRET=your-spotify-client-secret (linux shell) \n" +
+        "4. Create a playlist where you want your tracks to be added \n" + 
+        "    get the ID of that playlist by using the share button. \n" + 
+        "    The ID is the string that goes after https://open.spotify.com/playlist/ \n" + 
+        "    e.g.  in 'https://open.spotify.com/playlist/37i9dQZF1DX6YTj07PjLwE' the id is 37i9dQZF1DX6YTj07PjLwE  \n" + 
+        "5. The csv file expects the format used by shazam: Index,TagTime,Title,Artist,URL,TrackKey. We skip the first line.  \n"
 
         if len(os.Args) < 3 {
                 fmt.Println(helptext)
                 os.Exit(0)   
         }
 
-
-
         // first start an HTTP server
         http.HandleFunc("/callback", completeAuth)
         http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
                 log.Println("Got request for:", r.URL.String())
         })
+
         go func() {
                 err := http.ListenAndServe(":8080", nil)
                 if err != nil {
@@ -87,41 +85,39 @@ func main() {
         }
         fmt.Println("You are logged in as:", user.ID)
 
+        //read args from cmd-line
+        PlaylistId := os.Args[1]
+        Filename := os.Args[2]
 
-            PlaylistId := os.Args[1]
-            Filename := os.Args[2]
-
-            f, err := os.Open(Filename)
-            if err != nil {
+        //open csv file and read rows
+        f, err := os.Open(Filename)
+        if err != nil {
                 panic(err)
-            }
-            defer f.Close()
-            records, err := readSample(f)
-            if err != nil {
-                panic(err)
-            }
-                    
-
-
-    results, err := client.GetPlaylist(context.Background(), spotify.ID(PlaylistId))
-
-    if err != nil {
-                log.Fatal(err)
+        }
+        defer f.Close()
+        records, err := readSample(f)
+        if err != nil {
+        panic(err)
         }
 
+        //get details of playlist
+        results, err := client.GetPlaylist(context.Background(), spotify.ID(PlaylistId))
+
+        if err != nil {
+                log.Fatal(err)
+        }
    
-    fmt.Println("\n")
-    fmt.Println("Playlist Name:", results.Name)
-    fmt.Println("Playlist Description:", results.Description)
-    fmt.Println("Playlist TOTAL Tracks:", results.Tracks.Total)
-    fmt.Println("\n")
+        fmt.Println("\n")
+        fmt.Println("Playlist Name:", results.Name)
+        fmt.Println("Playlist Description:", results.Description)
+        fmt.Println("Playlist TOTAL Tracks:", results.Tracks.Total)
+        fmt.Println("\n")
 
         results_tracks, err := client.GetPlaylistTracks(context.Background(), spotify.ID(PlaylistId))
 
         if err != nil {
                 log.Fatal(err)
         }
-
 
         type track struct {
             albumname string
@@ -134,11 +130,8 @@ func main() {
         if results_tracks.Tracks != nil && len(results_tracks.Tracks) >0 {
 
                 for _, item := range results_tracks.Tracks  {
-                                                
                         tracks_in_playlist = append(tracks_in_playlist,track{item.Track.Album.Name,item.Track.Name, item.Track.ID} )
-
                 }
-
         
                 for page := 1; ; page++ {
                         err = client.NextPage(context.Background(), results_tracks)
@@ -151,10 +144,7 @@ func main() {
                         for _, item := range results_tracks.Tracks  {
                                 tracks_in_playlist = append(tracks_in_playlist,track{item.Track.Album.Name,item.Track.Name, item.Track.ID} )
                         }
-                
                 }
-
-
         }
 
         var tracklist []spotify.ID
@@ -165,40 +155,31 @@ func main() {
     
         
         for _, item := range records  {
-                
                 var doweappend int = 1
-
-            q:= "track:" + item[2] + " " + "artist:" +  item[3]
-
-
-            results, err := client.Search(context.Background(), q, spotify.SearchTypeTrack|spotify.SearchTypeArtist)
-            if err != nil {
+                q:= "track:" + item[2] + " " + "artist:" +  item[3]
+                results, err := client.Search(context.Background(), q, spotify.SearchTypeTrack|spotify.SearchTypeArtist)
+                if err != nil {
                     log.Fatal(err)
-            }
+                }
 
-
-            // handle search results
-            if results.Tracks != nil && len(results.Tracks.Tracks) >0 {
+                // handle search results
+                if results.Tracks != nil && len(results.Tracks.Tracks) >0 {
                 
-                for i := range tracks_in_playlist {
-                        
-                                    if ((tracks_in_playlist[i].albumname == results.Tracks.Tracks[0].Album.Name) && (tracks_in_playlist[i].trackname == results.Tracks.Tracks[0].Name )) {
-                                        // track already in playlist!
-                                        doweappend = 0
-                                        break
-                                    } 
-                                }
+                        for i := range tracks_in_playlist {
                                 
-                                if doweappend == 1 {                    
-                                        fmt.Println("NEW TRACK ::", "Album:",results.Tracks.Tracks[0].Album.Name, "----", "Track:",results.Tracks.Tracks[0].Name,"----", "ID:", results.Tracks.Tracks[0].ID )
-                                        tracklist = append(tracklist,  spotify.ID(results.Tracks.Tracks[0].ID)  )
-                                }
-               
-            }
-
+                                if ((tracks_in_playlist[i].albumname == results.Tracks.Tracks[0].Album.Name) && (tracks_in_playlist[i].trackname == results.Tracks.Tracks[0].Name )) {
+                                // track already in playlist!
+                                doweappend = 0
+                                break
+                                } 
+                        }
+                                        
+                        if doweappend == 1 {                    
+                                fmt.Println("NEW TRACK ::", "Album:",results.Tracks.Tracks[0].Album.Name, "----", "Track:",results.Tracks.Tracks[0].Name,"----", "ID:", results.Tracks.Tracks[0].ID )
+                                tracklist = append(tracklist,  spotify.ID(results.Tracks.Tracks[0].ID)  )
+                        }
+                }
         }
-
-
 
         if len(tracklist) == 0 {
                 fmt.Println("----Nothing to do. There aren't new tracks to add to the playlist." )
@@ -208,11 +189,8 @@ func main() {
                 fmt.Println("")
                 fmt.Println("There are: ", len(tracklist), " tracks to update in the playlist")
                 fmt.Println("----Let's add tracks via API ----" )
-       
-        }
+       }
         
-
-
         // Split the slice into batches of 100 items.
         batch := 100
 
@@ -226,29 +204,21 @@ func main() {
                 _, err := client.AddTracksToPlaylist(context.Background(), spotify.ID(PlaylistId), tracklist[i:j]...)
                 fmt.Print( len(tracklist[i:j]) , ",")
                 
-                    if err != nil {
-                        log.Fatal(err)
-                    }
-
+                if err != nil {
+                log.Fatal(err)
+                }
         }
                 
         fmt.Println("")
-                fmt.Println("----Tracks Updated!! ----" )
-                results_playlist_update, err := client.GetPlaylist(context.Background(), spotify.ID(PlaylistId))
+        fmt.Println("----Tracks Updated!! ----" )
+        results_playlist_update, err := client.GetPlaylist(context.Background(), spotify.ID(PlaylistId))
 
-            if err != nil {
-                        log.Fatal(err)
-                }
-
-           
-            fmt.Println("\n")
-            fmt.Println("Playlist TOTAL Tracks after the update:", results_playlist_update.Tracks.Total)
-            fmt.Println("\n")
-
-
-
-
-
+        if err != nil {
+                log.Fatal(err)
+        }
+        fmt.Println("\n")
+        fmt.Println("Playlist TOTAL Tracks after the update:", results_playlist_update.Tracks.Total)
+        fmt.Println("\n")
 }
 
 func completeAuth(w http.ResponseWriter, r *http.Request) {
